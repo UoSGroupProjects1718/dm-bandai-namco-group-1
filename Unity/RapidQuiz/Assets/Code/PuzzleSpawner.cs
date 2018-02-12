cbbb95e6018f9d52c7e633af0d58e0530f115a6e
Elliot Dewhurst <elliot+github@dewhurst.me>
2018-02-12 12:58:24 +0000

    Added initial prototype

---
+++ Unity/RapidQuiz/Assets/Code/PuzzleSpawner.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class PuzzleSpawner : MonoBehaviour
+{
+	public GameObject Puzzle1Area, Puzzle2Area;
+	public GameObject[] Puzzles;
+
+	// Use this for initialization
+	void Start ()
+	{
+		SpawnPuzzle(1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void SpawnPuzzle(int puzzleIndex)
+	{
+		var puzzlesToSpawn = Puzzles.SkipWhile(puzzle =>
+			!puzzle.name.Contains("Puzzle" + puzzleIndex.ToString())).ToArray();
+		if (puzzlesToSpawn.Count() == 2)
+		{
+			Instantiate(puzzlesToSpawn[0], Puzzle1Area.transform);
+			Instantiate(puzzlesToSpawn[1], Puzzle2Area.transform);
+		}
+		else
+		{
+			Debug.LogWarning("Only found " + puzzlesToSpawn.Count() + " puzzles!"); 
+		}
+	}
+}
